64b8262d3cec0e9dd40ce5eb98bbcc374df22493
Roy <spinosaurus001@hotmail.com>
2021-11-24 21:13:41 -0800

    Update RpgleFree.js
    Added a check to see if the RPG source code begins on Column 1 or Column 6. When on column 1 add  five spaces.

--- src/RpgleFree.js
+++ src/RpgleFree.js
@@ -78,14 +78,14 @@ module.exports = class RpgleFree {
       sourceVar.const = true;
     } else {
       switch (sourceVar.type) {
-      case `D`:
-        sourceVar.len = 10;
-        sourceVar.const = true;
-        break;
-      case `T`:
-        sourceVar.len = 8;
-        sourceVar.const = true;
-        break;
+        case `D`:
+          sourceVar.len = 10;
+          sourceVar.const = true;
+          break;
+        case `T`:
+          sourceVar.len = 8;
+          sourceVar.const = true;
+          break;
       }
     }
   
@@ -102,65 +102,64 @@ module.exports = class RpgleFree {
       let assignee = targetVar.name;
   
       switch (targetVar.type) {
-      case `S`: //numeric (not specific to packed or zoned)
-        result.value = assignee + ` = ` + sourceVar.name;
-        break;
-        
-      case `D`: //date
-        if (sourceVar.name.toUpperCase() === `*DATE`) {
-          result.value = targetVar.name + ` = ` + sourceVar.name;
-        } else {
-          if (obj.attr === ``)
-            result.value = targetVar.name + ` = %Date(` + sourceVar.name + `)`;
-          else
-            result.value = targetVar.name + ` = %Date(` + sourceVar.name + `:` + obj.attr + `)`;
-        }
-        break;
-  
-      case `A`: //character
-        if (obj.padded) {
-          if (obj.dir === `MOVEL`)
-            assignee = targetVar.name;
-          else
-            assignee = `EvalR ` + targetVar.name;
-        } else {
-          if (obj.dir === `MOVEL`)
-            if (sourceVar.const)
-              assignee = `%Subst(` + targetVar.name + `:1:` + sourceVar.len + `)`;
-            else
-              assignee = `%Subst(` + targetVar.name + `:1:%Len(` + sourceVar.name + `))`;
-          else
-          if (sourceVar.const)
-            assignee = `%Subst(` + targetVar.name + `:%Len(` + targetVar.name + `)-` + sourceVar.len + `)`;
-          else
-            assignee = `%Subst(` + targetVar.name + `:%Len(` + targetVar.name + `)-%Len(` + sourceVar.name + `))`;
-        }
-  
-        switch (sourceVar.type) {
-  
-        case `A`:
+        case `S`: //numeric (not specific to packed or zoned)
           result.value = assignee + ` = ` + sourceVar.name;
           break;
-  
-        case `S`:
-        case `P`:
-        case `I`:
-        case `F`:
-        case `U`:
-          result.value = assignee + ` = %Char(` + sourceVar.name + `)`;
+          
+        case `D`: //date
+          if (sourceVar.name.toUpperCase() === `*DATE`) {
+            result.value = targetVar.name + ` = ` + sourceVar.name;
+          } else {
+            if (obj.attr === ``)
+              result.value = targetVar.name + ` = %Date(` + sourceVar.name + `)`;
+            else
+              result.value = targetVar.name + ` = %Date(` + sourceVar.name + `:` + obj.attr + `)`;
+          }
           break;
-  
-        case `D`:
-        case `T`:
-          if (obj.attr !== ``)
-            result.value = assignee + ` = %Char(` + sourceVar.name + `:` + obj.attr + `)`;
-          else
+    
+        case `A`: //character
+          if (obj.padded) {
+            if (obj.dir === `MOVEL`)
+              assignee = targetVar.name;
+            else
+              assignee = `EvalR ` + targetVar.name;
+          } else {
+            if (obj.dir === `MOVEL`)
+              if (sourceVar.const)
+                assignee = `%Subst(` + targetVar.name + `:1:` + sourceVar.len + `)`;
+              else
+                assignee = `%Subst(` + targetVar.name + `:1:%Len(` + sourceVar.name + `))`;
+            else
+            if (sourceVar.const)
+              assignee = `%Subst(` + targetVar.name + `:%Len(` + targetVar.name + `)-` + sourceVar.len + `)`;
+            else
+              assignee = `%Subst(` + targetVar.name + `:%Len(` + targetVar.name + `)-%Len(` + sourceVar.name + `))`;
+          }
+    
+          switch (sourceVar.type) {
+    
+          case `A`:
+            result.value = assignee + ` = ` + sourceVar.name;
+            break;
+    
+          case `S`:
+          case `P`:
+          case `I`:
+          case `F`:
+          case `U`:
             result.value = assignee + ` = %Char(` + sourceVar.name + `)`;
-        }
-  
-        break;
+            break;
+    
+          case `D`:
+          case `T`:
+            if (obj.attr !== ``)
+              result.value = assignee + ` = %Char(` + sourceVar.name + `:` + obj.attr + `)`;
+            else
+              result.value = assignee + ` = %Char(` + sourceVar.name + `)`;
+          }
+    
+          break;
       }
-  
     }
   
     if (result.value !== ``) {
@@ -181,13 +180,20 @@ module.exports = class RpgleFree {
     let fixedSql = false;
     let lastBlock = ``;
     let index = 0;
+
     for (index = 0; index < length; index++) {
       if (this.lines[index] === undefined) continue;
+
+      // check if RPG source starts at col 6
+      // if not add space
+      if (this.lines[index].substr(0,5).trim !== "")
+        this.lines[index] = "     " + this.lines[index];
         
       this.currentLine = index;
   
       comment = ``;
       line = ` ` + this.lines[index].padEnd(80);
+
       if (line.length > 81) {
         line = line.substr(0, 81);
         comment = this.lines[index].substr(80);
@@ -205,51 +211,50 @@ module.exports = class RpgleFree {
       spec = line[6].toUpperCase();
 
       switch (line[7]) {
-      case `/`:
-        let test = line.substr(8,8).trim().toUpperCase();
-        switch (test) {
-        case `EXEC SQL`:
-          // deal with embedded SQL just like normal c-specs
-          fixedSql = true;
-          spec = `C`;
+        case `/`:
+          let test = line.substr(8,8).trim().toUpperCase();
+          switch (test) {
+          case `EXEC SQL`:
+            // deal with embedded SQL just like normal c-specs
+            fixedSql = true;
+            spec = `C`;
+            break;
+          case `END-EXEC`:  
+            // deal with embedded SQL just like normal c-specs
+            fixedSql = false;
+            spec = `C`;
+            break;
+          case `FREE`:
+          case `END-FREE`:
+            spec = ``;
+            this.lines.splice(index, 1);
+            index--;
+            break;
+          default:
+            spec = ``;
+            this.lines[index] = ``.padEnd(7) + ``.padEnd(spaces) + line.substr(7).trim();
+            break;
+          }
           break;
-        case `END-EXEC`:  
-          // deal with embedded SQL just like normal c-specs
-          fixedSql = false;
-          spec = `C`;
-          break;
-        case `FREE`:
-        case `END-FREE`:
-          spec = ``;
-          this.lines.splice(index, 1);
-          index--;
-          break;
-        default:
-          spec = ``;
-          this.lines[index] = ``.padEnd(7) + ``.padEnd(spaces) + line.substr(7).trim();
-          break;
-        }
-        break;
 
-      case `*`:
-        // check for RPG style comments
-        spec = ``;
-    
-        comment = line.substr(8).trim();
-        if (comment !== ``){
-          var idx = comment.indexOf("*") + 1;
-          this.lines[index] = ``.padEnd(8) + ``.padEnd(spaces) + `// ` + comment.substr(idx);
-        }
-        else
-          this.lines[index] = ``;
-        break;
+        case `*`:
+          // check for RPG style comments
+          spec = ``;
+      
+          comment = line.substr(8).trim();
+          if (comment !== ``){
+            var idx = comment.indexOf("*") + 1;
+            this.lines[index] = ``.padEnd(8) + ``.padEnd(spaces) + `// ` + comment.substr(idx);
+          }
+          else
+            this.lines[index] = ``;
+          break;
 
-      case `+`:
-        // deal with embedded SQL just like normal c-specs
-        if (fixedSql)
-          spec = `C`;
-        break;
-        
+        case `+`:
+          // deal with embedded SQL just like normal c-specs
+          if (fixedSql)
+            spec = `C`;
+          break;
       }
   
       if (specs[spec] !== undefined) {
@@ -279,70 +284,70 @@ module.exports = class RpgleFree {
         }
   
         switch (true) {
-        case isMove:
-          result = this.suggestMove(result.move);
-          if (result.change) {
-            this.lines[index] = ignoredColumns + `    ` + ``.padEnd(spaces) + result.value;
-          }
-          break;
-  
-        case hasKeywords:
-          let endStmti = this.lines[index - 1].indexOf(`;`);
-          let endStmt = this.lines[index - 1].substr(endStmti); //Keep those end line comments :D
-  
-          this.lines[index - 1] = this.lines[index - 1].substr(0, endStmti) + ` ` + result.aboveKeywords + endStmt;
-          this.lines.splice(index, 1);
-          index--;
-          break;
-  
-        case result.remove:
-          if (comment.trim() !== ``) {
-            this.lines[index] = ignoredColumns + `    ` + ``.padEnd(spaces) + `//` + comment;
-          } else {
+          case isMove:
+            result = this.suggestMove(result.move);
+            if (result.change) {
+              this.lines[index] = ignoredColumns + `    ` + ``.padEnd(spaces) + result.value;
+            }
+            break;
+    
+          case hasKeywords:
+            let endStmti = this.lines[index - 1].indexOf(`;`);
+            let endStmt = this.lines[index - 1].substr(endStmti); //Keep those end line comments :D
+    
+            this.lines[index - 1] = this.lines[index - 1].substr(0, endStmti) + ` ` + result.aboveKeywords + endStmt;
             this.lines.splice(index, 1);
             index--;
-            length++;
-          }
-          break;
-  
-        case result.change:
-          spaces += result.beforeSpaces;
-  
-          if (result.arrayoutput) {
-  
-            this.lines.splice(index, 1);
-  
-            for (let y in result.arrayoutput) {
-              result.arrayoutput[y] = ignoredColumns + `    ` + ``.padEnd(spaces) + result.arrayoutput[y];
-  
-              this.lines.splice(index, 0, result.arrayoutput[y]);
-              index++;
+            break;
+    
+          case result.remove:
+            if (comment.trim() !== ``) {
+              this.lines[index] = ignoredColumns + `    ` + ``.padEnd(spaces) + `// ` + comment;
+            } else {
+              this.lines.splice(index, 1);
+              index--;
               length++;
             }
-  
-            index--;
-  
-          } else {
+            break;
+    
+          case result.change:
+            spaces += result.beforeSpaces;
+    
+            if (result.arrayoutput) {
+    
+              this.lines.splice(index, 1);
+    
+              for (let y in result.arrayoutput) {
+                result.arrayoutput[y] = ignoredColumns + `    ` + ``.padEnd(spaces) + result.arrayoutput[y];
+    
+                this.lines.splice(index, 0, result.arrayoutput[y]);
+                index++;
+                length++;
+              }
+    
+              index--;
+    
+            } else {
 
-            this.lines[index] = ignoredColumns + `    ` + ``.padEnd(spaces) + result.value;
-            if (comment.trim() !== ``) {
-              this.lines[index] += ` //` + comment;
-            }
-            
-          }
+              this.lines[index] = ignoredColumns + `    ` + ``.padEnd(spaces) + result.value;
+              if (comment.trim() !== ``) {
+                this.lines[index] += ` //` + comment;
+              }
               
-          spaces += result.nextSpaces;
-          break;
+            }
+                
+            spaces += result.nextSpaces;
+            break;
         }
   
       } else {
         if (wasSub) {
-          endBlock(this.lines,this.indent);
+          endBlock(this.lines, this.indent);
         }
       }
     }
   
-    function endBlock(lines,indent) {
+    function endBlock(lines, indent) {
       spaces -= indent;
       if (lastBlock !== undefined) {
         lines.splice(index, 0, ``.padEnd(8) + ``.padEnd(spaces) + `End-` + lastBlock + `;`);
@@ -353,5 +358,4 @@ module.exports = class RpgleFree {
     }
 
   }
-  
 }
