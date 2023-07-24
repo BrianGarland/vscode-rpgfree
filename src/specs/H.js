7d3f4acf87af1cc3bdc9f8643b6d9d97a5ff625e
Firas al-Khalil <firasalkhalil@gmail.com>
2023-07-24 14:05:15 +0200

    H: convert to ES module

--- src/specs/H.js
+++ src/specs/H.js
@@ -1,21 +1,19 @@
-module.exports = {
-  Parse: function (input, indent, wasSub) {
-    let output = {
-      remove: false,
-      change: false,
-      value: '',
+export function Parse(input, indent, wasSub) {
+  let output = {
+    remove: false,
+    change: false,
+    value: '',
 
-      beforeSpaces: 0,
-      nextSpaces: 0
-    };
+    beforeSpaces: 0,
+    nextSpaces: 0
+  };
 
-    let keywords = input.substr(7);
-    output.value = `Ctl-Opt ${keywords.trim()}`;
+  let keywords = input.substr(7);
+  output.value = `Ctl-Opt ${keywords.trim()}`;
 
-    if (output.value !== '') {
-      output.change = true;
-      output.value = `${output.value.trimRight()};`;
-    }
-    return output;
+  if (output.value !== '') {
+    output.change = true;
+    output.value = `${output.value.trimRight()};`;
   }
+  return output;
 }
